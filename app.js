745ec222559bdda5c9349c204bf8f3c9efea2dd5
Bowen Jin <bowen.jin@vanderbilt.edu>
2017-01-07 23:43:31 -0600

    minor fix

--- app.js
+++ app.js
@@ -473,6 +473,7 @@ io.on('connection', function (socket) {
                 //     }, error_handler)
                 // }, 60000)
                 var user = active_users.get(user_id);
+                console.log(user);
                 var alert = user.first_name + " " + user.last_name + " is requesting to " +
                     (transaction.buy ? "sell " : "buy ") + transaction.title + " for " + transaction.price;
                 var notification_info = {alert: alert, category: "TRANSACTION_REQUEST_MADE", payload: {transaction_id: transaction._id.toString()}};
