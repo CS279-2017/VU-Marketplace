43b5b53911252dc1b8578e342ee1f27d0ae933fc
Chris Lee <christopher.p.lee@vanderbilt.edu>
2017-03-26 16:11:28 -0500

    deleted some uneeded code

--- WebApp/public/js/new_post.js
+++ WebApp/public/js/new_post.js
@@ -1,35 +1,5 @@
-<<<<<<< HEAD
 'use strict';
 
-$(document).ready(function() {
-
-
-        // $('form').submit(function(e) {
-        //     e.preventDefault();
-        //
-        //     // http://stackoverflow.com/questions/1184624/convert-form-data-to-javascript-object-with-jquery
-        //     const formData = {};
-        //     $(e.target).serializeArray().map(function(x){formData[x.name] = x.value;});
-        //
-        //     var today = new Date();
-        //     formData.startDate = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-        //     console.log(formData);
-        //
-        //     $.post('/v1/user/'+ formData.vunetid, formData)
-        //         .done((result) => {
-        //             console.log("Ajax called successful new post");
-        //             window.location = 'listings.html';
-        //             // localStorage.setItem('username', result.username);
-        //             // localStorage.setItem('email', result.primary_email);
-        //         })
-        //         .fail((err) => {
-        //             alert('New post failed');
-        //             // localStorage.clear();
-        //             console.error(err);
-        //         });
-        //
-        // });
-});
 
 //inject angular file upload directives and services.
 var app = angular.module('fileUpload', ['ngFileUpload']);
