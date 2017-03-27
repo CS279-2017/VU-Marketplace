6782650b5aeca4b246a5dd6ab5198ba6ffb349e1
Chris Lee <christopher.p.lee@vanderbilt.edu>
2017-03-26 20:44:13 -0500

    update

--- WebApp/public/js/new_post.js
+++ WebApp/public/js/new_post.js
@@ -36,21 +36,21 @@ app.controller('MyCtrl', ['$scope', 'Upload', '$timeout', function ($scope, Uplo
 
 }]);
 
-=======
-/**
- * Created by Githiora_Wamunyu on 3/13/2017.
- */
-import Post from
 
-function createNewPost() {
-
-    let productName = $('#productName').val();
-    let radioValue = $("input[name=optradio]:checked").val();
-    let itemDescription = $('#TextArea').val();
-    let price = $('#price').val();
-    let deliveryMethod = $('#deliveryMethod').val();
-
-    Post.title = productName
-    Post
-}
+// /**
+//  * Created by Githiora_Wamunyu on 3/13/2017.
+//  */
+// import Post from
+//
+// function createNewPost() {
+//
+//     let productName = $('#productName').val();
+//     let radioValue = $("input[name=optradio]:checked").val();
+//     let itemDescription = $('#TextArea').val();
+//     let price = $('#price').val();
+//     let deliveryMethod = $('#deliveryMethod').val();
+//
+//     Post.title = productName
+//     Post
+// }
 
