6f93f8111f6f38476a8288a6386c73dd4dde107f
Jon Lee <jonathan.k.lee@vanderbilt.edu>
2017-04-01 15:45:56 -0500

    added register js

--- WebApp/public/js/register.js
+++ WebApp/public/js/register.js
@@ -20,3 +20,25 @@
 //
 // });
 
+$(document).ready(function() {
+    $('login-form').submit(function(e) {
+        e.preventDefault();
+
+        // http://stackoverflow.com/questions/1184624/convert-form-data-to-javascript-object-with-jquery
+        const formData = {};
+        $(e.target).serializeArray().map(function(x){formData[x.name] = x.value;});
+
+        $.post('/v1/session', formData)
+            .done((result) => {
+                window.location = 'listings.html';
+                localStorage.setItem('username', result.vunetid);
+                localStorage.setItem('email', result.primary_email);
+            })
+            .fail((err) => {
+                alert('Could not authenticate user. Please try again.');
+                localStorage.removeItem('username');
+                localStorage.removeItem('email');
+                console.error(err);
+            });
+    });
+});
